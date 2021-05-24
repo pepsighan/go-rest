12e3733903eacaf8421dda4f2decae39fcf9bcea
Sharad Chand <pepsighan@sharadchand.com>
2021-05-24 10:04:33 +0545

    Setup the docker package to be deployed on cloud run

--- main.go
+++ main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	// Determine port for HTTP service.
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "1323"
+		log.Printf("defaulting to port %s", port)
+	}
+
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	e.Logger.Fatal(e.Start(":1323"))
+	e.Logger.Fatal(e.Start(":" + port))
 }
